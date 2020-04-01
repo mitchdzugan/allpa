ab3c236cbd6db71d2d0ff0c791b4c159e5454187
Mitch Dzugan <mitchdzugan@gmail.com>
2020-04-01 01:27:15 -0500

    update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject allpa "0.1.1"
-            :description "FIXME: write description"
-            :url "http://example.com/FIXME"
-            :license {:name "Eclipse Public License"
-                      :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "FIXME: write description"
+  :url "https://github.com/mitchdzugan/allpa"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies
   [[org.clojure/clojure "1.10.0" :scope "provided"]
    [org.clojure/clojurescript "1.10.597" :scope "provided"]]
